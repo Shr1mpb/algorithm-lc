abb1112537033260347179b055e745e293915bd2
Shr1mp <1205874457@qq.com>
2025-07-21 19:11:20 +0800

    完成5题 2025.7.21

---
+++ src/main/java/GroupAnagrams.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+/**
+ * 字母异位词分组
+ */
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = new String(charArray);
+            if (map.containsKey(sortedStr)) {
+                map.get(sortedStr).add(str);
+            }else{
+                map.put(sortedStr, new ArrayList<>());
+                map.get(sortedStr).add(str);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}
