7e77181315dad1a2bbc3955e0cf48a364f7221e2
Shr1mp <1205874457@qq.com>
2025-07-23 12:20:37 +0800

    完成5题 2025.7.23 单调队列orTreeMap(n log k)(滑动窗口最大值)、前缀和(最大子数组和)

---
+++ src/main/java/MaxSubArray.java
@@ -0,0 +1,23 @@
+/**
+ * 最大子数组和
+ * 前缀思想 能拿到每段数组的和 然后维护最大
+ */
+public class MaxSubArray {
+    public int maxSubArray(int[] nums) {
+        int pre = 0;
+        int preMin = 0;
+        int maxSum = Integer.MIN_VALUE;
+        for (int num : nums) {
+            // 先更新前缀最小值 再更新前缀
+            preMin = Math.min(pre, preMin);
+            pre += num;
+            maxSum = Math.max(maxSum, pre - preMin);
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        MaxSubArray maxSubArray = new MaxSubArray();
+        maxSubArray.maxSubArray(new int[]{-1,-2});
+    }
+}
