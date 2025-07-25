1473f8c756c6a52c00ec39ca3c72cd8b464b314b
Shr1mp <1205874457@qq.com>
2025-07-25 11:25:26 +0800

    完成5题 2025.7.25 链表Floyd 反转链表(遍历改指向)

---
+++ src/main/java/CycleLinkedList.java
@@ -0,0 +1,25 @@
+/**
+ * 环形链表
+ * // TODO
+ */
+public class CycleLinkedList {
+    // Floyd 龟兔赛跑法
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+    private class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
