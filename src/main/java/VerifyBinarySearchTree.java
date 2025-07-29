be1378782cae002bcc7ef0b6ed3df5b290bfd1d9
Shr1mp <1205874457@qq.com>
2025-07-29 10:05:43 +0800

    完成5题 2025.7.29 二叉树相关 递归+BFS

---
+++ src/main/java/VerifyBinarySearchTree.java
@@ -0,0 +1,15 @@
+/**
+ * 验证二叉搜索树
+ * // TODO
+ */
+public class VerifyBinarySearchTree {
+    public boolean isValidBST(TreeNode root) {
+        return verify(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean verify(TreeNode root, Long lower, Long upper) {
+        if (root == null) return true;
+        if (root.val <= lower || root.val >= upper) return false;
+        return verify(root.left, lower, Integer.valueOf(root.val).longValue()) && verify(root.right,Integer.valueOf(root.val).longValue(), upper);
+    }
+}
