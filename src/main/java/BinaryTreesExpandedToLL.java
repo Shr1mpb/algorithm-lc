953288f380d16fcf703d15bd13515ef7fb13cbd5
Shr1mp <1205874457@qq.com>
2025-07-30 12:05:29 +0800

    完成5题 2025.7.30 DFS不熟练

---
+++ src/main/java/BinaryTreesExpandedToLL.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 二叉树展开为链表
+ */
+public class BinaryTreesExpandedToLL {
+    Queue<TreeNode> q = new LinkedList<TreeNode>();
+    public void flatten(TreeNode root) {
+        inorder(root);
+        // 遍历队列 设置指针
+        while (!q.isEmpty()) {
+            TreeNode node = q.poll();
+            node.right = q.peek();
+            node.left = null;
+        }
+    }
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+        q.offer(root);
+        inorder(root.left);
+        inorder(root.right);
+    }
+}
---
+++ src/main/java/NearestCommonAncestorOfBinaryTree.java
@@ -0,0 +1,13 @@
+/**
+ * 二叉树的最近公共祖先
+ * // TODO
+ */
+public class NearestCommonAncestorOfBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // 向下寻找p/q 之后向上拼接
+        if(root == null || root == p || root == q) return root;
+        TreeNode l = lowestCommonAncestor(root.left , p , q);
+        TreeNode r = lowestCommonAncestor(root.right , p , q);
+        return l == null ? r : (r == null ? l : root);
+    }
+}
