08f6f4e611f31b3af6b68ce2f2141e2116d89e08
Shr1mp <1205874457@qq.com>
2025-08-06 14:03:46 +0800

    完成5题 2025.8.6 三道栈难题(2每日温度单调栈) 二道堆简单

---
+++ src/main/java/EverydayTemperature.java
@@ -0,0 +1,24 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 每日温度
+ * // TODO 单调栈
+ */
+public class EverydayTemperature {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int length = temperatures.length;
+        int[] ans = new int[length];
+        Deque<Integer> stack = new LinkedList<Integer>();
+        for (int i = 0; i < length; i++) {
+            int temperature = temperatures[i];
+            while (!stack.isEmpty() && temperature > temperatures[stack.peek()]) {
+                // 比之前的大 更新之前的答案天数 并弹出更新后的天
+                int prevIndex = stack.pop();
+                ans[prevIndex] = i - prevIndex;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
