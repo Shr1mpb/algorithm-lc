6d11bfdc7284cedafa84918a562cdd869357f8f7
Shr1mp <1205874457@qq.com>
2025-08-07 11:34:57 +0800

    完成5题 2025.8.7 堆1贪心4 跳一跳II 贪心往往伴随遍历 复习后贪心整体重做

---
+++ src/main/java/DivideLetterIntervals.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 划分字母区间
+ * // TODO 记录最后出现的字母的下标 尽可能多的包含片段
+ */
+public class DivideLetterIntervals {
+    public List<Integer> partitionLabels(String s) {
+        int[] last = new int[26];
+        int length = s.length();
+        // 记录所有字母最后出现的下标
+        for (int i = 0; i < length; i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+        List<Integer> partition = new ArrayList<Integer>();
+        int start = 0, end = 0;
+        // 每次记录最后索引的位置 然后不断更新 到最后索引的位置时说明这个片段已经完毕 加入长度
+        for (int i = 0; i < length; i++) {
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+            if (i == end) {
+                partition.add(end - start + 1);
+                start = end + 1;
+            }
+        }
+        return partition;
+    }
+}
---
+++ src/main/java/MedianFinder.java
@@ -0,0 +1,37 @@
+import java.util.PriorityQueue;
+
+/**
+ * 数据流的中位数
+ * // TODO 大小顶堆
+ */
+class MedianFinder {
+
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+    public MedianFinder() {
+        minHeap = new PriorityQueue<Integer>(); // 小顶堆，存大的那一半（那么堆顶元素就是大半里面最小的那个，即最接近中心的）
+        maxHeap = new PriorityQueue<Integer>((x, y) -> y - x);// 大顶堆，存小的那一半（那么堆顶元素就是小半里面最大的那个，即最接近中心的）
+    }
+
+    public void addNum(int num) {
+        if (minHeap.isEmpty() || num > minHeap.peek()) {// 如果新数字大于小顶堆的堆顶元素（即当前"较大的一半"中的最小值），说明这个数字确实属于较大的那一半，所以放入小顶堆。
+            minHeap.offer(num);
+        } else {
+            maxHeap.offer(num);
+        }
+        // 这里确保minHeap 和 maxHeap要么一样多 要么是minHeap多一个
+        if (minHeap.size() > maxHeap.size() + 1) {
+            maxHeap.offer(minHeap.poll());
+        } else if (maxHeap.size() > minHeap.size()) {
+            minHeap.offer(maxHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (minHeap.size() == maxHeap.size()) {
+            return (minHeap.peek() + maxHeap.peek()) / 2.0;
+        } else {
+            return minHeap.peek();
+        }
+    }
+}
